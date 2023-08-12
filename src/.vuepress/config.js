180dad03788e89dc0ba029131142964ed6b080fb
xiehui <xie18960920183@outlook.com>
2023-08-12 19:27:24 +0800

    fix: home title

--- src/.vuepress/config.js
+++ src/.vuepress/config.js
@@ -4,7 +4,7 @@ module.exports = {
   /**
    * Ref：https://v1.vuepress.vuejs.org/config/#title
    */
-  title: 'AIoT',
+  title: '民工笔记',
   /**
    * Ref：https://v1.vuepress.vuejs.org/config/#description
    */
